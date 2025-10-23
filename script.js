1eb6e91a9827c4d5622d35a592b5b332cbc4fa60
KriptoBaba382 <199w29c@mail.ru>
2025-10-23 17:15:19 +0300

    add: btn danger & removeNote

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Счетчик'
+            placeholderString: 'Введите название заметки',
+            title: 'Список заметок',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
 
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNewNote() {
+            this.notes.push(this.inputValue)
+            this.inputValue = ''
+        },
+        removeNote(idx) {
+            this.notes.splice(idx, 1)
         }
     }
 }
