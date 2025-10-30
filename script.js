026cbeafc1fa11323edf5e0123062429cfa755d7
KriptoBaba382 <199w29c@mail.ru>
2025-10-30 17:52:42 +0300

    add class

--- script.js
+++ script.js
@@ -4,21 +4,39 @@ const App = {
             placeholderString: 'Введите название заметки',
             title: 'Список заметок',
             inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2']
-
+            notes: ['Заметка 1', 'Заметка 2'],
+            inputType: 'password'
         }
     },
     methods: {
-        inputChangeHandler(event) {
-            this.inputValue = event.target.value
-        },
+
         addNewNote() {
-            this.notes.push(this.inputValue)
-            this.inputValue = ''
+            if (this.inputValue !== "") {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+
+        toUpperCase(item) {
+            return item.toUpperCase()
         },
         removeNote(idx) {
             this.notes.splice(idx, 1)
+        },
+    },
+    computed: {
+        doubleCountComputed() {
+            console.log('doubleCountComputed')
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = ''
+            }
         }
     }
+
 }
 const app = Vue.createApp(App).mount('#app')
