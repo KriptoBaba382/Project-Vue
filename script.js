f3050d6a236499c62065536a986f6c1e2494919d
KriptoBaba382 <199w29c@mail.ru>
2025-11-05 18:00:56 +0300

    add

--- script.js
+++ script.js
@@ -1,42 +1,11 @@
-const App = {
-    data() {
-        return {
-            placeholderString: 'Введите название заметки',
-            title: 'Список заметок',
-            inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2'],
-            inputType: 'password'
+Vue.createApp({
+    data: () => ({
+        myHtml: '<h1>Vue 3 App</h1>',
+        title: 'Я есть Грут',
+        person: {
+            firstName: 'Vladilen',
+            lastName: 'Minin',
+            age: 27
         }
-    },
-    methods: {
-
-        addNewNote() {
-            if (this.inputValue !== "") {
-                this.notes.push(this.inputValue)
-                this.inputValue = ''
-            }
-        },
-
-        toUpperCase(item) {
-            return item.toUpperCase()
-        },
-        removeNote(idx) {
-            this.notes.splice(idx, 1)
-        },
-    },
-    computed: {
-        doubleCountComputed() {
-            console.log('doubleCountComputed')
-            return this.notes.length * 2
-        },
-    },
-    watch: {
-        inputValue(value) {
-            if (value.length > 10) {
-                this.inputValue = ''
-            }
-        }
-    }
-
-}
-const app = Vue.createApp(App).mount('#app')
+    })
+}).mount('#app')
